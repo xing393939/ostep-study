fcff3bfc6abb26ca06ccae6f1f8693cadfffbac3
xing393939 <xing393939@users.noreply.github.com>
2021-04-20 16:41:08 +0800

    3

--- code/test_asm.c
+++ code/test_asm.c
@@ -18,3 +18,33 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+
+int
+mon_backtrace(int argc, char **argv, struct Trapframe *tf) {
+    // Your code here.
+    uint32_t ebp = read_ebp();
+    uint32_t eip = 0;
+    struct Eipdebuginfo info;
+    #define TO_INT(x)  *((uint32_t*)(x))
+    while (ebp) {
+        eip = TO_INT((ebp + 4));
+        cprintf("ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",
+                ebp,                /*ebp*/
+                eip,                /*eip*/
+                TO_INT((ebp + 8)),    /*arg1*/
+                TO_INT((ebp + 12)),   /*arg2*/
+                TO_INT((ebp + 16)),   /*arg3*/
+                TO_INT((ebp + 20)),   /*arg4*/
+                TO_INT((ebp + 24)));  /*arg5*/
+
+        if (!debuginfo_eip(eip, &info)) {
+            cprintf("%s:%d: %.*s+%d\n",
+                    info.eip_file,
+                    info.eip_line,
+                    info.eip_fn_namelen, info.eip_fn_name,
+                    eip - info.eip_fn_addr);
+        }
+        ebp = TO_INT(ebp);
+    }
+    return 0;
+}
