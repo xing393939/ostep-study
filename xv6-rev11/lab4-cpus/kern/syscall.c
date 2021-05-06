5ab90da52d4b7cf852abe197ada3969b0b64b08b
xing393939 <xing393939@users.noreply.github.com>
2021-05-06 16:28:33 +0800

    练习 7

--- xv6-rev11/lab4-cpus/kern/syscall.c
+++ xv6-rev11/lab4-cpus/kern/syscall.c
@@ -347,6 +347,14 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
         case SYS_env_destroy:
             assert(curenv);
             return sys_env_destroy(sys_getenvid());
+        case SYS_page_alloc:
+            return sys_page_alloc((envid_t)a1, (void * )a2, (int )a3);
+        case SYS_page_map:
+            return sys_page_map((envid_t) a1, (void *) a2, (envid_t) a3, (void *) a4, (int) a5);
+        case SYS_page_unmap:
+            return sys_page_unmap((envid_t) a1, (void *) a2);
+        case SYS_exofork:
+            return sys_exofork();
         case SYS_yield:
             sys_yield();
             return 0;
