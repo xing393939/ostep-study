5a5ec7fd32029d368ac7502a30cb1ebfb5521e2b
chenxing <chenxing@bookan.com.cn>
2021-05-07 17:07:24 +0800

    练习 14

--- xv6-rev11/lab4-cpus/user/spin.c
+++ xv6-rev11/lab4-cpus/user/spin.c
@@ -8,9 +8,9 @@ umain(int argc, char **argv)
 {
 	envid_t env;
 
-	cprintf("I am the parent.  Forking the child...\n");
+	cprintf("I am the parent %08x.  Forking the child...\n", thisenv->env_id);
 	if ((env = fork()) == 0) {
-		cprintf("I am the child.  Spinning...\n");
+		cprintf("I am the child %08x.  Spinning...\n", env);
 		while (1)
 			/* do nothing */;
 	}
