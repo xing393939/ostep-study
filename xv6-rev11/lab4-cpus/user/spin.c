a2be659562428f2d3a7755b26c175791ff194c64
chenxing <chenxing@bookan.com.cn>
2021-05-07 17:10:31 +0800

    1

--- xv6-rev11/lab4-cpus/user/spin.c
+++ xv6-rev11/lab4-cpus/user/spin.c
@@ -8,14 +8,14 @@ umain(int argc, char **argv)
 {
 	envid_t env;
 
-	cprintf("I am the parent %08x.  Forking the child...\n", thisenv->env_id);
+	cprintf("I am the parent.  Forking the child...\n");
 	if ((env = fork()) == 0) {
-		cprintf("I am the child %08x.  Spinning...\n", env);
+		cprintf("I am the child.  Spinning...\n");
 		while (1)
 			/* do nothing */;
 	}
 
-	cprintf("I am the parent.  Running the child...\n");
+	cprintf("I am the parent %08x.  Running the child %08x...\n", thisenv->env_id, env);
 	sys_yield();
 	sys_yield();
 	sys_yield();
