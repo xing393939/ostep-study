b6d45bf5ba163db3e692e2c8252d53353c4fa586
xing393939 <xing393939@users.noreply.github.com>
2021-05-12 14:43:10 +0800

    练习 7

--- xv6-rev11/lab5-file/kern/syscall.c
+++ xv6-rev11/lab5-file/kern/syscall.c
@@ -141,7 +141,19 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+    struct Env *theEnv;
+    if (envid2env(envid, &theEnv, 1) < 0)
+        return -E_BAD_ENV;
+    user_mem_assert(theEnv, tf, sizeof(struct Trapframe), PTE_U);
+    // Clear IOPL bits.
+    tf->tf_eflags &= ~FL_IOPL_MASK;
+    // Interrupts enabled.
+    tf->tf_eflags |= FL_IF;
+    // code protection level 3(CPL 3).
+    tf->tf_cs |= GD_UT | 3;
+    // set envid's trap frame to tf.
+    theEnv->env_tf = *tf;
+    return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -434,6 +446,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
             return sys_ipc_try_send((envid_t) a1, (uint32_t) a2, (void *) a3, (unsigned) a4);
         case SYS_ipc_recv:
             return sys_ipc_recv((void *) a1);
+        case SYS_env_set_trapframe:
+            return sys_env_set_trapframe((envid_t) a1, (Trapframe) a2);
         default:
             return -E_INVAL;
     }
