0f2a78da4d22815d64a95b60443cc7ccedcb2cb9
chenxing <chenxing@bookan.com.cn>
2021-05-17 15:29:39 +0800

    练习 4

--- xv6-rev11/lab6-net/kern/e1000.c
+++ xv6-rev11/lab6-net/kern/e1000.c
@@ -3,13 +3,12 @@
 #include <kern/pci.h>
 
 // LAB 6: Your driver code here
-#define E1000_LOCATE(offset)  (offset >> 2)
-volatile uint32_t *e1000;
+volatile void *bar_va;
 
 int
-pci_e1000_attach(struct pci_func *pcif) {
+e1000_attachfn(struct pci_func *pcif) {
     pci_func_enable(pcif);
-    e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-    cprintf("device status:[%08x]\n", e1000[E1000_LOCATE(E1000_DEVICE_STATUS)]);
+    bar_va = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("device status:[%08x]\n", (uint32_t *) E1000REG(E1000_STATUS));
     return 0;
 }
