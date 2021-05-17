abb06aef791f4bb31c80e07d4f6e84f793a0c8fc
chenxing <chenxing@bookan.com.cn>
2021-05-17 11:36:42 +0800

    练习 3

--- xv6-rev11/lab6-net/kern/e1000.c
+++ xv6-rev11/lab6-net/kern/e1000.c
@@ -1,9 +1,10 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
+#include <kern/pci.h>
 
 // LAB 6: Your driver code here
 int
-e1000_attachfn(struct pci_func *pcif) {
+e1000_attach(struct pci_func *pcif) {
     pci_func_enable(pcif);
     return 0;
 }
