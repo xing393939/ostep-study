080d52d2294db9211901d85a44f4c40cbdac77c1
xing393939 <xing393939@users.noreply.github.com>
2021-05-17 11:58:35 +0800

    1

--- xv6-rev11/lab6-net/kern/e1000.h
+++ xv6-rev11/lab6-net/kern/e1000.h
@@ -8,4 +8,47 @@ pci_e1000_attach(struct pci_func *pcif);
 #define PCI_E1000_VENDOR_ID 0x8086
 #define PCI_E1000_DEVICE_ID 0x100E
 
+/* Register Set */
+#define E1000_DEVICE_STATUS   0x00008  /* Device Status - RO */
+
+#define E1000_ICS      0x000C8  /* Interrupt Cause Set - WO */
+#define E1000_IMS      0x000D0  /* Interrupt Mask Set - RW */
+
+#define E1000_RCTL     0x00100  /* RX Control - RW */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+
+#define E1000_RDBAL    0x02800  /* RX Descriptor Base Address Low - RW */
+#define E1000_RDBAH    0x02804  /* RX Descriptor Base Address High - RW */
+#define E1000_RDLEN    0x02808  /* RX Descriptor Length - RW */
+#define E1000_RDH      0x02810  /* RX Descriptor Head - RW */
+#define E1000_RDT      0x02818  /* RX Descriptor Tail - RW */
+#define E1000_RA       0x05400  /* Receive Address - RW Array */
+
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0X03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+
+/* Transmit Control */
+#define E1000_TCTL_RST    0x00000001    /* Reserved */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_BCE    0x00000004    /* Reserved */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+#define E1000_TCTL_SWXOFF 0x00400000    /* SW Xoff transmission */
+#define E1000_TCTL_PBE    0x00800000    /* Reserved */
+#define E1000_TCTL_RTLC   0x01000000    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU   0x02000000    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR   0x10000000    /* Reserved */
+
+
+#define E1000_RCTL_EN     0x00000002    /* enable */
+#define E1000_RCTL_BAM    0x00008000    /* broadcast enable */
+#define E1000_RCTL_SECRC  0x04000000    /* Strip Ethernet CRC */
+
 #endif  // SOL >= 6
