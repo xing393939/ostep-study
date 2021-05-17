f1a9dd025177c923f0e21a396f31f5454a320565
chenxing <chenxing@bookan.com.cn>
2021-05-17 11:33:25 +0800

    1

--- xv6-rev11/lab6-net/kern/e1000.c
+++ xv6-rev11/lab6-net/kern/e1000.c
@@ -2,3 +2,8 @@
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+int
+e1000_attachfn(struct pci_func *pcif) {
+    pci_func_enable(pcif);
+    return 0;
+}
--- xv6-rev11/lab6-net/kern/e1000.h
+++ xv6-rev11/lab6-net/kern/e1000.h
@@ -1,3 +1,10 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+int
+e1000_attachfn(struct pci_func *pcif);
+
+#define PCI_E1000_VENDOR_ID 0x8086
+#define PCI_E1000_DEVICE_ID 0x100E
+
 #endif  // SOL >= 6
