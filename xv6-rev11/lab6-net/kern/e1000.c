30a20408fba738a11902acccd76109e2e1669bf0
chenxing <chenxing@bookan.com.cn>
2021-05-17 15:42:27 +0800

    1

--- xv6-rev11/lab6-net/kern/e1000.c
+++ xv6-rev11/lab6-net/kern/e1000.c
@@ -9,6 +9,6 @@ int
 e1000_attachfn(struct pci_func *pcif) {
     pci_func_enable(pcif);
     bar_va = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-    cprintf("device status:[%08x]\n", (uint32_t *) E1000REG(E1000_STATUS));
+    cprintf("device status:[%08x]\n", E1000REG(E1000_STATUS));
     return 0;
 }
