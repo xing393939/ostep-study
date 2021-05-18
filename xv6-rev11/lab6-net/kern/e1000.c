06422519cb1e37d8f28f631397a94a2ef98c16e5
xing393939 <xing393939@users.noreply.github.com>
2021-05-18 16:29:39 +0800

    练习 11

--- xv6-rev11/lab6-net/kern/e1000.c
+++ xv6-rev11/lab6-net/kern/e1000.c
@@ -85,7 +85,70 @@ e1000_transmit(void *data, size_t len) {
     return 0;
 }
 
+static void
+get_ra_address(uint32_t mac[], uint32_t *ral, uint32_t *rah) {
+    uint32_t low = 0, high = 0;
+    int i;
+
+    for (i = 0; i < 4; i++) {
+        low |= mac[i] << (8 * i);
+    }
+
+    for (i = 4; i < 6; i++) {
+        high |= mac[i] << (8 * i);
+    }
+
+    *ral = low;
+    *rah = high | E1000_RAH_AV;
+}
+
+static void
+e1000_receive_init() {
+    //RDBAL and RDBAH register
+    uint32_t *rdbal = (uint32_t *) E1000REG(E1000_RDBAL);
+    uint32_t *rdbah = (uint32_t *) E1000REG(E1000_RDBAH);
+    *rdbal = PADDR(rx_desc_array);
+    *rdbah = 0;
+
+    int i;
+    for (i = 0; i < RXDESCS; i++) {
+        rx_desc_array[i].addr = PADDR(rx_buffer_array[i]);
+    }
+    //RDLEN register
+    struct e1000_rdlen *rdlen = (struct e1000_rdlen *) E1000REG(E1000_RDLEN);
+    rdlen->len = RXDESCS;
+
+    //RDH and RDT register
+    rdh = (struct e1000_rdh *) E1000REG(E1000_RDH);
+    rdt = (struct e1000_rdt *) E1000REG(E1000_RDT);
+    rdh->rdh = 0;
+    rdt->rdt = RXDESCS - 1;
+
+    uint32_t *rctl = (uint32_t *) E1000REG(E1000_RCTL);
+    *rctl = E1000_RCTL_EN | E1000_RCTL_BAM | E1000_RCTL_SECRC;
+
+    uint32_t *ra = (uint32_t *) E1000REG(E1000_RA);
+    uint32_t ral, rah;
+    get_ra_address(E1000_MAC, &ral, &rah);
+    ra[0] = ral;
+    ra[1] = rah;
+}
+
 int
 e1000_receive(void *addr, size_t *len) {
+    static int32_t next = 0;
+    if (!(rx_desc_array[next].status & E1000_RXD_STAT_DD)) {
+        //simply tell client to retry
+        return -E_RECEIVE_RETRY;
+    }
+    if (rx_desc_array[next].errors) {
+        cprintf("receive errors\n");
+        return -E_RECEIVE_RETRY;
+    }
+    *len = rx_desc_array[next].length;
+    memcpy(addr, rx_buffer_array[next], *len);
+
+    rdt->rdt = (rdt->rdt + 1) % RXDESCS;
+    next = (next + 1) % RXDESCS;
     return 0;
 }
