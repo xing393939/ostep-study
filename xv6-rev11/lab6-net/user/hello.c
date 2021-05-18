8e19fee2e70644ff670180ba10cc0908f4e624a5
xing393939 <xing393939@users.noreply.github.com>
2021-05-18 09:10:20 +0800

    hello

--- xv6-rev11/lab6-net/user/hello.c
+++ xv6-rev11/lab6-net/user/hello.c
@@ -2,9 +2,25 @@
 #include <inc/lib.h>
 
 void
-umain(int argc, char **argv)
-{
-	cprintf("hello, world\n");
-	char *content = "abcd";
-	cprintf("sys_pkt_send: %d\n", sys_pkt_send(content, 5));
+sleep(int sec) {
+    unsigned now = sys_time_msec();
+    unsigned end = now + sec * 1000;
+
+    if ((int) now < 0 && (int) now > -MAXERROR)
+        panic("sys_time_msec: %e", (int) now);
+    if (end < now)
+        panic("sleep: wrap");
+
+    while (sys_time_msec() < end)
+        sys_yield();
+}
+
+void
+umain(int argc, char **argv) {
+    cprintf("hello, world\n");
+    char *content = "abcd";
+    for (int i = 0; i < 50; i++) {
+        sleep(1);
+        cprintf("sys_pkt_send: %d\n", sys_pkt_send(content, 5));
+    }
 }
