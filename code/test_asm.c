e0fb4a8ba16b08079829abf894fec50bc35d8bd4
chenxing <chenxing@bookan.com.cn>
2022-09-23 17:11:25 +0800

    1

--- code/test_asm.c
+++ code/test_asm.c
@@ -1,20 +1,55 @@
-#include<stdio.h>
-#include<malloc.h>
-#include<unistd.h>
+// Server side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-void test_mov() {
-    int a = 10, b;
-    asm ("movl %1, %%eax;"
-         "movl %%eax, %0;"
-    :"=r"(b)          /* output 该字段的语法后面会详细说明，此处可无视，下同 */
-    :"r"(a)           /* input   */
-    :"%eax"           /* clobbered register */
-    );
-    printf("b=%d\n", b);
-}
+#define PORT     5000
+#define MAXLINE 1024
 
-int main(int argc, char **argv) {
-    test_mov();
+// Driver code
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    char *hello = "Hello from server";
+    struct sockaddr_in servaddr, cliaddr;
 
-    return 0;
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *) &servaddr,
+             sizeof(servaddr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        int len, n;
+        len = sizeof(cliaddr);  //len is value/result
+        n = recvfrom(sockfd, (char *) buffer, MAXLINE,
+                     MSG_WAITALL, (struct sockaddr *) &cliaddr,
+                     &len);
+        buffer[n] = '\0';
+        printf("Client : %s\n", buffer);
+        sendto(sockfd, (const char *) hello, strlen(hello),
+               MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+               len);
+        printf("Hello message sent.\n");
+    }
 }
